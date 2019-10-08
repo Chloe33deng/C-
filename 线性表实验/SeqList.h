494ec4fd88e840ef735916937b23999449b694a6
Admin <yuanying@xmut.edu.cn>
2019-10-08 23:21:13 +0800

    学生提交1

---
+++ 线性表实验/SeqList.h
@@ -0,0 +1,18 @@
+# ifndef SeqList_H
+# define SeqList_H
+    const int MaxSize = 10;
+    class SeqList{
+        public:
+            SeqList(){length=0;};
+            SeqList(int a[],int n);
+            ~SeqList(){};
+            void Insert(int i,int x);
+            int Delete(int i);
+            int Locate(int x);
+            void PrintList();
+            int Length();
+        private:
+            int data[MaxSize];
+            int length;
+    };
+# endif
